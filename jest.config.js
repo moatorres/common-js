3e3ce039ab8650961e80f615731bcb3e095134a0
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-05-28 22:39:17 -0300

    update: improved config

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'node',
+  testRegex: '\\.(test|spec)?\\.(js|jsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['jest-extended'],
+}
