7ad15fe09c74577e842d4515626122947ce114c4
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-02 14:20:39 -0300

    update: minor changes

--- src/utils/after.js
+++ src/utils/after.js
@@ -1,3 +1,5 @@
+import isFunction from './is-function'
+
 /**
  * The opposite of `before`. This method creates a function that invokes
  * `fn` once it's called `n` or more times.
@@ -7,17 +9,11 @@
  * @returns {Function} Returns the new restricted function.
  *
  */
-function after(n, fn) {
-  if (typeof fn !== 'function') {
-    throw new TypeError('Expected a function (after)')
-  }
-
-  n = n || 0
+function after(n = 0, fn) {
+  if (!isFunction(fn)) throw new TypeError('Expected a function (after)')
 
   return function (...args) {
-    if (--n < 1) {
-      return fn.apply(this, args)
-    }
+    if (--n < 1) return fn.apply(this, args)
   }
 }
 
