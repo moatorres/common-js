bffd292df93da3fa686f466dbf48bba51e1330d8
Moa Torres <44585769+moatorres@users.noreply.github.com>
2021-07-29 12:46:51 -0300

    update: minor improvements

--- src/utils/after.spec.js
+++ src/utils/after.spec.js
@@ -1,17 +1,27 @@
+// @ts-nocheck
 import after from './after'
 
-describe('Utilities after()', () => {
-  it('Should restrict calls to `fn`', () => {
-    let arr = []
-    let myFn = () => arr.push(1)
-    let execMyFn = after(4, myFn)
-    execMyFn() // []
-    execMyFn() // []
-    execMyFn() // []
-    execMyFn() // arr = [ 1 ]
-    execMyFn() // arr = [ 1, 1 ]
-    execMyFn() // arr = [ 1, 1, 1 ]
+describe('after()', () => {
+  it('Should be defined', () => {
+    expect(after).toBeDefined()
+  })
 
-    expect(arr.length).toEqual(3)
+  it('Should only execute on the "nth" call to `fn`', () => {
+    let arr = []
+
+    let click = () => arr.push(1)
+
+    let doClick = after(4, click)
+
+    doClick() // 1st click => []
+    doClick() // 2nd click => []
+    doClick() // 3rd click => []
+    doClick() // 4th click => [ 1 ] // → first execution :: after(4, arr.push(1))
+    doClick() // 5th click => [ 1, 1 ]
+    doClick() // 6th click => [ 1, 1, 1 ]
+    doClick() // 7th click => [ 1, 1, 1, 1 ]
+
+    console.log(arr)
+    expect(arr.length).toEqual(4) // => [ 1, 1, 1, 1 ]
   })
 })
